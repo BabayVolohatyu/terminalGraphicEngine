bdc271010ae7ded1f541341d6576200b0783139d
Babay <babayvolohatyu@gmail.com>
2025-04-30 17:50:38 +0300

    Add Terminal interface

---
+++ include/terminal/terminal.h
@@ -0,0 +1,18 @@
+#ifndef TERMINAL_H
+#define TERMINAL_H
+
+#include <iostream>
+namespace terminal {
+
+class Terminal {
+  protected:
+    virtual void rescale(double scale) = 0;
+
+    virtual void resize(int rows, int cols) = 0;
+
+    static std::string getCommandOutput(const char* cmd);
+
+    virtual ~Terminal() = default;
+};
+}
+#endif
