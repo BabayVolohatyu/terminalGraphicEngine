887612e87b777153b5c5e7d91f4e8d40da7d51e7
Babay <babayvolohatyu@gmail.com>
2025-04-30 23:28:04 +0300

    Add Camera class header

---
+++ include/engine/camera.h
@@ -0,0 +1,103 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include <math/vector3.h>
+#include <math/quaternion.h>
+
+#include <engine/object.h>
+
+using namespace math;
+
+namespace engine{
+
+class Camera{
+  private:
+    Vector3 _position; // Camera position in world space
+    Quaternion _rotation; // Camera orientation
+    double _fov; // Field of view (in degrees)
+    double _aspectRatio; // Aspect ratio (width / height)
+    double _nearClip; // Near clipping plane
+    double _farClip; // Far clipping plane
+
+    static Matrix<double> translate(const Vector3 &position);
+
+    static void display(int screenX, int screenY);
+
+    void drawLine(int x0, int y0, int x1, int y1) const;
+
+    void drawVertices(const Object &object, int screenWidth, int screenHeight) const;
+
+    void drawTriangle(const std::pair<double, double> &p1,
+                  const std::pair<double, double> &p2,
+                  const std::pair<double, double> &p3) const;
+
+    void drawTriangles(const Object &object, int screenWidth, int screenHeight) const;
+
+  public:
+    const Vector3 worldForward{0, 0, -1};
+
+    const Vector3 worldUp{0, 1, 0};
+
+    const Vector3 worldRight{1, 0, 0};
+
+    //add dependency between angles and those three vectors
+    Vector3 forward{0, 0, -1};
+
+    Vector3 up{0, 1, 0};
+
+    Vector3 right{1, 0, 0};
+
+    Camera(const Vector3 &position = {0, 0, 0},
+           const Quaternion &rotation = {0, 0, 0},
+           double fov = 90,
+           double aspectRatio = 16 / 9,
+           double nearClip = 0.1,
+           double farClip = 1000);
+
+    //function accepts two parameters for its variants that work accordingly to camera rotation
+    void translatePosition(const Vector3 &direction, double amount);
+
+    void moveForward(double amount);
+
+    void moveRight(double amount);
+
+    void moveUp(double amount);
+
+    void lookAt(const Point &point);
+
+    void rotate(const Vector3 &eulerAngles);
+
+    Matrix<double> viewMatrix() const;
+
+    Matrix<double> projectionMatrix() const;
+
+    //draws only triangles for now
+    void drawObject(const Object &object, int screenWidth, int screenHeight) const;
+
+    Vector3 getPosition() const;
+
+    Quaternion getRotation() const;
+
+    double getFov() const;
+
+    double getAspectRatio() const;
+
+    double getNearClip() const;
+
+    double getFarClip() const;
+
+    void setPosition(const Vector3 &position);
+
+    void setRotation(const Quaternion &rotation);
+
+    void setFov(double fov);
+
+    void setAspectRatio(double aspectRatio);
+
+    void setNearClip(double nearClip);
+
+    void setFarClip(double farClip);
+};
+}
+
+#endif
