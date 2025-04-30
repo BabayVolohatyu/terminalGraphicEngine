4d647ff243ec56f44b7cbd9996d0ef32b9155b41
Babay <babayvolohatyu@gmail.com>
2025-04-30 21:53:17 +0300

    Add Quaternion class header

---
+++ include/math/quaternion.h
@@ -0,0 +1,65 @@
+#ifndef QUATERNION_H
+#define QUATERNION_H
+
+#include <math/vector3.h>
+#include <math/matrix.h>
+
+namespace math {
+
+class Quaternion {
+  private:
+    double w_, x_, y_, z_;
+
+    void approximate(double &w, double &x, double &y, double &z);
+
+    double approximate(double value);
+
+    double degreesToRadians(double degrees);
+  public:
+    Quaternion();
+
+    explicit Quaternion(double w = 0.0, double x = 0.0, double y = 0.0, double z = 0.0);
+
+    Quaternion(double w, const Vector3 &rotation);
+
+    explicit Quaternion(const Vector3 &rotation);
+
+    Quaternion(double xRotation, double yRotation, double zRotation);
+
+    ~Quaternion() = default;
+
+    static Matrix<double> toMatrix(const Quaternion& quaternion);
+
+    static Quaternion fromAxis(const Vector3 &axis, double angle);
+
+    [[nodiscard]] Quaternion normalized() const;
+
+    [[nodiscard]] Quaternion conjugated() const;
+
+    [[nodiscard]] double magnitude() const;
+
+    Quaternion operator*(const Quaternion &other) const noexcept;
+
+    Vector3 operator*(const Vector3 &v) const;
+
+    Quaternion &operator*=(const Quaternion &other) noexcept;
+
+    [[nodiscard]] double getW() const;
+
+    [[nodiscard]] double getX() const;
+
+    [[nodiscard]] double getY() const;
+
+    [[nodiscard]] double getZ() const;
+
+    void setW(double w);
+
+    void setX(double x);
+
+    void setY(double y);
+
+    void setZ(double z);
+};
+}
+
+#endif
